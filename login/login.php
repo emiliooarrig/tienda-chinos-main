b337d712520da5e7359ac0ec01712de1de96d2fe
emilio <earriaga@anahuac.mx>
2024-06-24 17:05:13 -0600

    Creamos el login para editar los articulos

--- login/login.php
+++ login/login.php
@@ -12,9 +12,28 @@
 
 
     <div class="container">
-
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <h2 class="text-center">Login</h2>
+                <form action="login_process.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    <?php
+                    if (isset($_GET['error'])) {
+                        echo '<div class="alert alert-danger mt-3">' . htmlspecialchars($_GET['error']) . '</div>';
+                    }
+                    ?>
+                </form>
+            </div>
+        </div>
     </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
