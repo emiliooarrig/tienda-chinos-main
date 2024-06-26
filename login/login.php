fc01f3769f9b687362cbd5af4f7ff0f12246992e
emilio <earriaga@anahuac.mx>
2024-06-26 13:11:22 -0600

    Agregamos el navbar; modificamos el formulario de login

--- login/login.php
+++ login/login.php
@@ -10,24 +10,42 @@
 
 <body>
 
-
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <h2 class="text-center">Login</h2>
-                <form action="../config/validar.php" method="post">
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" name="username" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" required>
-                    </div>
-                    <button name ="btningresar" type="submit" class="btn btn-primary btn-block">Login</button>
-                </form>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"> Tienda </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="../index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../articulos/articulos.php">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+        <h2 class="text-center">Login</h2>
+        <form action="../config/validar.php" method="post" autocomplete="off">
+            <div class="mb-3">
+                <label class="form-label" for="username"> User </label>
+                <input type="text" name="username" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="password"> Contraseña </label>
+                <input type="password" name="password" class="form-control" required>
+            </div>
+            <button name="btningresar" type="submit" class="btn btn-primary btn-block">Login</button>
+        </form>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
