56df5050254f6bf86d676567d95a30e521e41e89
emilio <earriaga@anahuac.mx>
2024-06-27 01:22:17 -0600

    Mostramos los artículos en la seccion de administrador

--- admin/administrar.php
+++ admin/administrar.php
@@ -1,9 +1,20 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['user'])) {
     header('Location: ../login/login.php');
     exit;
 }
+
+require '../config/conection.php';
+$db = new Database();
+
+$con = $db->conectar();
+
+$sql = $con->prepare("SELECT id, nombre , precio , descripcion FROM arts");
+$sql->execute();
+
+$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 <!DOCTYPE html>
@@ -21,11 +32,30 @@ if (!isset($_SESSION['user'])) {
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <h2 class="text-center">Bienvenido</h2>
-                <p class="text-center">Esta es una página protegida.</p>
                 <a href="../config/logout.php" class="btn btn-danger btn-block">Cerrar sesión</a>
             </div>
         </div>
     </div>
+
+    <div class="container">
+        <h1> Productos publicados: </h1>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <!-- Mostramos todos los resultados en la base de datos -->
+            <?php foreach ($resultado as $row) { ?>
+                <div class="col" style="margin-top: 10px;">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"> <?php echo $row["nombre"]; ?> </h5>
+                            <h6 class="card-subtitle"> Precio: $<?php echo $row["precio"] ?></h6>
+                            <p class="card-text"> Descripcion: <?php echo $row["descripcion"] ?> </p>
+                            <a href="editar_art.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
 </body>
 
 </html>
