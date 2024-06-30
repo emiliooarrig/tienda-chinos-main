2c64dee9e31a7e34d084f2a06af49a80a5e741a2
emiliooarrig <earriaga@anahuac.mx>
2024-06-30 12:14:29 -0600

    Creamos el footer en contacto

--- contacto/contacto.php
+++ contacto/contacto.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Contacto</title>
 </head>
 
 <body>
@@ -44,7 +44,7 @@
         <div class="row">
             <h1> Whatsapp</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Ab dolores nisi perspiciatis ea. Error maxime voluptate eos, vitae similique enim laudantium eligendi, magnam sint a qui corrupti quaerat harum porro.
             </p>
         </div>
@@ -52,7 +52,11 @@
     </div>
 
 
-
+    <footer class="container-fluid text-center bg-light">
+        <p class="fw-bolder">
+            Tienda XXXXXXX. Todos los derechos reservados
+        </p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
