ed1d16d092afa14814c720d4653f334cd9693861
emilio <earriaga@anahuac.mx>
2024-07-07 17:13:49 -0600

    Agregamos icono al boton de login

--- articulos/articulos.php
+++ articulos/articulos.php
@@ -19,6 +19,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Articulos</title>
 </head>
@@ -44,7 +45,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                         <a class="nav-link" href="#">Contacto</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../login/login.php">Login</a>
+                        <a class="nav-link" href="../login/login.php"> <i class="bi bi-person-circle"></i></a>
                     </li>
                 </ul>
             </div>
